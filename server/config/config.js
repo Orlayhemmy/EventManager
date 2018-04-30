9143de3d0f8e1ae0a4a8b34f8662b07dd8c9ba09
Orlayhemmy <daminomics@gmail.com>
2018-04-30 17:32:26 +0100

    fix travis build

--- server/config/config.js
+++ server/config/config.js
@@ -10,12 +10,13 @@ module.exports = {
     port: 5432,
   },
   test: {
-    username: 'postgres',
-    password: 'profyem001',
-    database: 'centermanager_test',
-    host: '127.0.0.1',
-    port: 5432,
-    dialect: 'postgres'
+    // username: 'postgres',
+    // password: 'profyem001',
+    // database: 'centermanager_test',
+    // host: '127.0.0.1',
+    // port: 5432,
+    dialect: 'postgres',
+    database: process.env.TESTDB_URL
   },
   production: {
     use_env_variable: 'DATABASE_URL',
