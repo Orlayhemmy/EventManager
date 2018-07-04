0d559a6e865d8df93b86a739c75056ac2af65f95
Orlayhemmy <daminomics@gmail.com>
2018-07-04 13:25:17 +0100

    exclude sass file

--- server/helper/sendMail.js
+++ server/helper/sendMail.js
@@ -7,37 +7,34 @@ import generateShortCode from './generateCode';
  * @returns {object} success or failure message
  */
 export default function sendMail(req, res) {
-  const {
-    email,
-    message,
-    title,
-  } = req.body;
+  const { email, message, title } = req.body;
   let shortCode;
   let codeTitle;
   let codeMessage;
   if (!message) {
     shortCode = generateShortCode();
-    codeMessage =`This code expires in 5 minutes <br/> code: <b> ${shortCode} </b>`
-    codeTitle = "Password Reset Code";
+    codeMessage = `This code expires in 5 minutes <br/> code:
+    <b> ${shortCode} </b>`;
+    codeTitle = 'Password Reset Code';
   }
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'ecenter.2017.andela@gmail.com',
-      pass: 'qwerty001?',
-    },
+      pass: 'qwerty001?'
+    }
   });
   const mailOptions = {
     from: 'ecenter.2017.andela@gmail.com',
     to: email,
     subject: title || codeTitle,
 
-    html: message || codeMessage,
+    html: message || codeMessage
   };
   transporter.sendMail(mailOptions, (error, info) => {
-    console.log(shortCode)
+    console.log(shortCode);
     if (error) {
-      return (error, info);
+      return error, info;
     }
     return res.status(201).send({
       message: 'Mail sent',
